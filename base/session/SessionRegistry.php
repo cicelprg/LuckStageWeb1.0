2222a322ef7c14d049bad68fa616fc37fabb7834
菜鸟贵 <scyaprg@163.com>
2014-05-07 17:15:18 +0800

    添加函数destroy

--- base/session/SessionRegistry.php
+++ base/session/SessionRegistry.php
@@ -14,46 +14,64 @@ class SessionRegistry extends Registry
 {
 	private static $instance;
 	
-	private function __construct()
-	{
+	private function __construct(){
 		ob_start();
+		ob_flush();
 		ob_clean();
 		session_start();
 	}
 	
-	static function instance()
-	{
-		if(!isset(self::$instance))
-		{
+	/**
+	 * 获取单例
+	 * @return \base\session\SessionRegistry
+	 */
+	static function instance(){
+		if(!isset(self::$instance)){
 			self::$instance=new self();
 		}
 		return self::$instance;
 	}
 	
-	protected function get($key)
-	{
-		return isset($_SESSION[__CLASS__][$key])?$_SESSION[__CLASS__][$key]:null;
+	/**
+	 * (non-PHPdoc)
+	 * @see \base\Registry::get()
+	 */
+	protected function get($key){
+		return isset($_SESSION[__CLASS__][$key])?unserialize($_SESSION[__CLASS__][$key]):null;
 	}
-	
-	protected function set($key, $val)
-	{
-		$_SESSION[__CLASS__][$key] = $val;
+	/**
+	 * (non-PHPdoc)
+	 * @see \base\Registry::set()
+	 */
+	protected function set($key, $val){
+		$_SESSION[__CLASS__][$key] = serialize($val);
 	}
 	
 	/**
+	 * 销毁一个session值
+	 * @param string $key
+	 */
+	protected function destroy($key){
+		unset($_SESSION[__CLASS__][$key]);
+	}
+	/**
 	 * 获取用户
 	 */
-	static function getUser()
-	{
+	static function getUser(){
 		return self::instance()->get('User');
 	}
 	
 	/**
 	 * 设定用户
 	 */
-	static function setUser($val)
-	{
+	static function setUser($val){
 		self::instance()->set('User', $val);
+	}
+	/**
+	 * 注销用户
+	 */
+	static function destroyUser(){
+		self::instance()->destroy('User');
 	}
 }
 
